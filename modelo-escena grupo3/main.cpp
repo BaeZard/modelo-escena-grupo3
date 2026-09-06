986433a5b933fd98735e627d7ec2796b6c816915
Belenn333 <kiaraferh@gmail.com>
2026-09-05 22:34:49 -0500

    Primer actualizacion

--- modelo-escena grupo3/main.cpp
+++ modelo-escena grupo3/main.cpp
@@ -1,7 +1,41 @@
-#include <iostream>
+#include <GL/freeglut.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void dibujar()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
 
-int main(int argc, char** argv) {
-	return 0;
+
+    
+    glutSwapBuffers();
 }
+
+void inicializar()
+{
+    glClearColor(1.0f, 0.75f, 0.8f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glOrtho(0, 400, 0, 400, -200, 200);
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("MODELO CUARTO 2D");
+
+    inicializar();
+
+    glutDisplayFunc(dibujar);
+
+    glutMainLoop();
+    return 0;
+}
+
+
